b1907877e08957884dd51f9ca3b7f1b47ddad150
angelheredia138 <herediafangel@gmail.com>
2024-06-04 19:47:36 -0700

    deleted tetris from app.jsx

--- retro-games-js/src/App.jsx
+++ retro-games-js/src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Pong from "./pages/Pong";
-import Tetris from "./pages/Tetris";
 import Game2048 from "./pages/Game2048";
 import Snake from "./pages/Snake";
 import Minesweeper from "./pages/Minesweeper";
@@ -13,7 +12,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/pong" element={<Pong />} />
-        <Route path="/tetris" element={<Tetris />} />
         <Route path="/2048" element={<Game2048 />} />
         <Route path="/snake" element={<Snake />} />
         <Route path="/minesweeper" element={<Minesweeper />} />
