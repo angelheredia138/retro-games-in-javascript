3a43e77c0bdf248e9f36afaa14198ce20268a5ef
angelheredia138 <herediafangel@gmail.com>
2024-06-05 21:38:51 -0700

    website functional, gotta update portfolio soon

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App";
 import "./index.css";
 
 ReactDOM.render(
-  <Router>
+  <Router basename={"/retro-games-in-javascript/"}>
     <App />
   </Router>,
   document.getElementById("root")
